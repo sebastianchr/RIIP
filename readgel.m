37be2b6d64bd8dd2f3e7b082ad0406143b9f08e7
sebastianchr <sebastian@chem.au.dk>
2015-12-08 13:37:13 +0100

    Revert "Integrate powder diffraction data of image plate"
    This reverts commit bcd5648b822f02bb09959b0667042a89630db8c6.

--- readgel.m
+++
@@ -1,4 +0,0 @@
-function intensity=readgel(filename)
-            RawDataIP = imread(filename);
-            intensity = single(RawDataIP).^2/42948;
-end
--- readtif.m
+++
@@ -1,4 +0,0 @@
-function intensity=readtif(filename)
-            RawDataIP = imread(filename);
-            intensity = (single(RawDataIP)+0.5)*(2^16-1)/42948;
-end
